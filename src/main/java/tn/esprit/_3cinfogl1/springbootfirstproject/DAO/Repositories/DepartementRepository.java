011131259482c8b5f800eadf6f7329c4f46087f9
Hanen-Azzouz <hanen-azzouz@hotmail.com>
2022-12-05 23:35:05 +0100

    pushing Rest Controllers & sql

--- src/main/java/tn/esprit/_3cinfogl1/springbootfirstproject/DAO/Repositories/DepartementRepository.java
+++ src/main/java/tn/esprit/_3cinfogl1/springbootfirstproject/DAO/Repositories/DepartementRepository.java
@@ -3,6 +3,13 @@ package tn.esprit._3cinfogl1.springbootfirstproject.DAO.Repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit._3cinfogl1.springbootfirstproject.DAO.Entities.Departement;
+
+import java.util.List;
+
 @Repository
 public interface DepartementRepository extends CrudRepository<Departement,Long> {
+
+
+//select * from departement where nom_departement=(valeur passée en paramétre)
+  List<Departement> getDepartementByNomDepartement(String nomDepartement);
 }
--- src/main/java/tn/esprit/_3cinfogl1/springbootfirstproject/DAO/Repositories/EtudiantRepository.java
+++ src/main/java/tn/esprit/_3cinfogl1/springbootfirstproject/DAO/Repositories/EtudiantRepository.java
@@ -2,7 +2,16 @@ package tn.esprit._3cinfogl1.springbootfirstproject.DAO.Repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import tn.esprit._3cinfogl1.springbootfirstproject.DAO.Entities.Departement;
 import tn.esprit._3cinfogl1.springbootfirstproject.DAO.Entities.Etudiant;
+
+import java.util.List;
+
 @Repository
 public interface EtudiantRepository extends CrudRepository<Etudiant,Long> {
+
+//select * from departement where nom_departement=(valeur passée en paramétre)
+
+
+
 }
